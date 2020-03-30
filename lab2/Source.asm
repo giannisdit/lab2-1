8be20dce96b6dee78ab25c585f37e276bc93d3b6
Γίαννης <@LAPTOP-GFRG3E5P>
2020-03-30 19:52:31 +0300

    askisi1

--- lab2/Source.asm
+++ lab2/Source.asm
@@ -3,7 +3,7 @@ INCLUDE Irvine32.inc
 
 .data
 mCalculating	BYTE	"Calculating...",0dh,0ah,0
-mAccessElement	BYTE	"The value of the element a[2,2 is:",0
+mAccessElement	BYTE	"The value of the element a[2,2] is:",0
 mResultSum		BYTE	"The sum a[2,2]+z= ;",0
 mResultDiff		BYTE	"The sum a[2,2]-z= ;",0
 array1 SDWORD 00d,10d,20d,30d
@@ -17,6 +17,36 @@ z SWORD	-30d
 
 .code
 main PROC
+		; ASKISI 1
+	mov edx, OFFSET mCalculating
+	call WriteString
+	movzx	ebx,x
+	shl		ebx,2
+	movzx	ecx,y
+	add		ebx,ecx
+	shl		ebx,2
+	mov		eax,[array1+ebx]
+	mov		edx,OFFSET mAccessElement
+	call	WriteString
+	call	WriteInt
+	call	Crlf
+
+	movsx	ecx,z
+	add		eax,ecx
+	mov		edx,OFFSET mResultSum
+	call	WriteString
+	call	WriteInt
+	call	Crlf
+
+	mov		eax,[array1+ebx]
+	sub		eax,ecx
+	mov		edx,OFFSET mResultDiff
+	call	WriteString
+	call	WriteInt
+	call	Crlf
+
+		; ASKISI 2
+
 
 	exit
 main ENDP
