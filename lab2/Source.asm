de52b23b5d592d234b089a5b1f2fd9b71823db69
Γίαννης <@LAPTOP-GFRG3E5P>
2020-03-30 19:34:50 +0300

    Declare variables

--- lab2/Source.asm
+++ lab2/Source.asm
@@ -2,6 +2,18 @@ TITLE Expression calculator
 INCLUDE Irvine32.inc
 
 .data
+mCalculating	BYTE	"Calculating...",0dh,0ah,0
+mAccessElement	BYTE	"The value of the element a[2,2 is:",0
+mResultSum		BYTE	"The sum a[2,2]+z= ;",0
+mResultDiff		BYTE	"The sum a[2,2]-z= ;",0
+array1 SDWORD 00d,10d,20d,30d
+ElementPerRow = ($ - array1)/TYPE array1
+	   SDWORD 01d,11d,21d,31d
+	   SDWORD 02d,12d,22d,32d
+	   SDWORD 03d,13d,23d,33d
+x bYTE	2d
+y BYTE	2d
+z SWORD	-30d 
 
 .code
 main PROC
